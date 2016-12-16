0d1ef709026c2a43d4c8b2786cdff704cd024625
harinivaskumar <harinivaskumar@live.com>
2016-12-17 03:30:06 +0530

    Code Commit
    Initial code commit

---
+++ app/src/main/java/com/hari/aag/contextmenu/ContextMenuActivity.java
@@ -0,0 +1,13 @@
+package com.hari.aag.contextmenu;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ContextMenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_context_menu);
+    }
+}
